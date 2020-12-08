d4c15fbf4c6006f9526837a4bded554980d57f80
Pandaaa2507 <Flo.h_Panda@gmx.at>
2020-12-08 15:24:42 +0100

    fixed path bug

--- config.js
+++ config.js
@@ -8,14 +8,6 @@ module.exports = {
             '313207271872135168'
         ]
     },
-    'commandLevels': {
-        '0': 'everyone',
-        '1': 'premium',
-        '2': 'developer',
-        '3': 'team',
-        '4': 'admin',
-        '5': 'owner'
-    },
     'errors': {
         '401': 'not permitted',
         '404': 'not found'
--- util/Core.js
+++ util/Core.js
@@ -1,5 +1,5 @@
 const DJSF = require('../DJSF');
-require('./Pad');
+require('./functions/pad');
 
 const Discord = require('discord.js');
 const fs = require('fs');
